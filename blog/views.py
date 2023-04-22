e615a8ea1b720793a78b11bea15f410831fd4e8d
quanghai <nguyenquanghai2331998@gmail.com>
2023-04-22 23:18:40 +0700

    Change App

--- blog/views.py
+++ blog/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-def index(request):
-    return render(request, 'home/index.html')
+def blog(request):
+    return render(request, 'blog/blog.html')
